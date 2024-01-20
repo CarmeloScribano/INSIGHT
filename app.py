94587826127d841dc0f9a2250551cacd6522082b
CarmeloS <carm.scrib@gmail.com>
2024-01-20 12:23:43 -0500

    Made the folder graphs work in

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
-# Import packages
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
-import pandas as pd
-import plotly.express as px
+from dash import Dash, html, dcc
+from graphs.carmGraph import fig
 
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-
-# Initialize the app
 app = Dash(__name__)
 
-# App layout
-app.layout = html.Div([
-    html.Div(children='My First App with Data, Graph, and Controls'),
-    html.Hr(),
-    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='my-final-radio-item-example'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='my-final-graph-example')
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
 ])
 
-# Add controls to build the interaction
-@callback(
-    Output(component_id='my-final-graph-example', component_property='figure'),
-    Input(component_id='my-final-radio-item-example', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
-
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
