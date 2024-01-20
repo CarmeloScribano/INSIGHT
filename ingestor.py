00aa939949cff6450a7814643b3919ddc3371817
kkliro <konstantin-klir@outlook.com>
2024-01-20 12:45:44 -0500

    Added return to ingestor.

--- ingestor.py
+++ ingestor.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-def get_dataframe(exchange_json):
+def get_data_frame(exchange_json):
     df = pd.read_json(f'data/{exchange_json}.json')
-    print(df)
-
-print(get_dataframe("Exchange_2"))
+    return df
 
 
     
