5fcb5d344314ecc7be477551085ec21208994afe
kkliro <konstantin-klir@outlook.com>
2024-01-20 17:39:22 -0500

    Added aggregated volume for treemap, as well as improved accuracy of the data

--- graphs/treemap.py
+++ graphs/treemap.py
@@ -1,32 +1,48 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 from ingestor import get_data_frame
-from utils import get_trades_by_type
+from utils import get_trades_by_type, get_df_rows_by_symbol
+
+pd.options.mode.copy_on_write = True
 
 df = get_data_frame("Exchange_1")
 
 def get_acked_trades():
     return get_trades_by_type(df, "NewOrderAcknowledged")
 
-def get_graph_data(): 
+def get_merged_df(left, right, key):
+    return pd.merge(left, right, on=key)
+
+def get_volume_df():
     symbol_counts = get_acked_trades()['Symbol'].value_counts()
 
-    # Create a new DataFrame for Plotly treemap
     treemap_data = pd.DataFrame({'Symbol': symbol_counts.index, 'Count': symbol_counts.values})
+    return treemap_data
 
+def get_graph_data(): 
+    treemap_data = get_volume_df()
     # Create a treemap using Plotly Express with animation
     fig = px.treemap(treemap_data, path=['Symbol'], values='Count', title='Symbol Frequency Treemap')
     fig.update_traces(hovertemplate='<b>Stock:</b> %{label}<br><b>Volume Traded:</b> %{value}')
     return fig
 
 def get_line_graph_data(selected_symbol):
-    # Filter data based on the selected symbol
-    filtered_data = get_acked_trades()[get_acked_trades()['Symbol'] == selected_symbol]
+    stock_data = get_acked_trades()
+    target_df = get_df_rows_by_symbol(stock_data, selected_symbol)
 
-    # Create a line graph for 30s intervals
-    line_fig = px.line(filtered_data, x='TimeStamp', y='OrderPrice', title=f'Line Graph for Symbol: {selected_symbol}')
-    return line_fig
+    target_df['TimeStamp'] = pd.to_datetime(target_df['TimeStamp'])
+
+    target_df.set_index('TimeStamp', inplace=True)
+
+    target_df_copy = target_df.copy()
+
+    resampled_df = target_df_copy.resample('5s').size().reset_index(name='TradeCount')
+
+    fig = go.Figure(data=go.Scatter(x=resampled_df['TimeStamp'], y=resampled_df['TradeCount'], mode='lines'))
+
+    return fig
 
 app = Dash(__name__)
 
@@ -49,6 +65,7 @@ def update_line_graph(click_data):
     if click_data is not None:
         # Extract the selected symbol from the click data
         selected_symbol = click_data['points'][0]['label']
+        print(selected_symbol)
 
         # Get line graph data for the selected symbol
         line_fig = get_line_graph_data(selected_symbol)
--- graphs/utils.py
+++ graphs/utils.py
@@ -1,2 +1,5 @@
 def get_trades_by_type(df, msg_type):
     return df[df['MessageType'] == msg_type]
+
+def get_df_rows_by_symbol(df, symbol):
+    return df[df['Symbol'] == symbol]
