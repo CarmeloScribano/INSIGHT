015aaa72f9b06a35fce981d13a2e4f9870c9a321
Ali Lezzeik <56936736+aliL123@users.noreply.github.com>
2024-01-20 17:38:39 -0500

    Transforming to data frame

--- graphs/stock_price.py
+++ graphs/stock_price.py
@@ -2,29 +2,33 @@ from ingestor import get_data_frame
 from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px
 import pandas as pd
-from utils import get_trades_by_type
-myDict = {}
+
 df = get_data_frame("Exchange_1")
 
-def get_cancel_request_trades(symbol):
+def get_cancel_request_trades(symbol, request,ack):
+    myDict = {}
     dff = df[df['Symbol'] == symbol]
     for ind in dff.index:
         order_id = df['OrderID'][ind]
-        if df['MessageType'][ind] == "CancelRequest" or df['MessageType'][ind] == "CancelAcknowledged":
-            time_stamp = df['TimeStamp'][ind]
+        if df['MessageType'][ind] == request or df['MessageType'][ind] == ack:
+            time_stamp = pd.to_datetime(df['TimeStamp'][ind])
             message_type = df['MessageType'][ind]
             subDict = {'TimeStamp':time_stamp,"MessageType":message_type}
             if order_id not in myDict:  
                 myDict[order_id] = [subDict]
             else:
                  myDict[order_id].append(subDict)
-    return dff  
+    return myDict  
 
 
-get_cancel_request_trades("PRQ83")
-count = 0
-for key, index in myDict.items():
-    if count == 0:
-        print(key,index)
-    count += 1
+
+def create_df_from_dict(trades_dict):
+
+    df = pd.DataFrame(columns=['TimeStarted',"Duration"])
+    for key, value in trades_dict.items():
+       duration = (value[0]['TimeStamp'].timestamp())
+       print(duration)
+        
+maDict = get_cancel_request_trades("PRQ83","CancelRequest","CancelAcknowledged")
+create_df_from_dict(maDict)
 
