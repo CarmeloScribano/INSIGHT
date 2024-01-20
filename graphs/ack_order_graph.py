b9690d38f6e5f0f1f8a5311241266019bc167884
SevagSA <sevagared2002@gmail.com>
2024-01-20 18:03:28 -0500

    created order ack duration functionality

---
+++ graphs/ack_order_graph.py
@@ -0,0 +1,21 @@
+import json
+from ingestor import get_data_frame
+from utils import get_trades_by_type
+
+
+def get_exchange_name():
+    return "Exchange_1"
+
+
+df = get_data_frame("Exchange_1")
+
+new_order_requests = get_trades_by_type(df=df, msg_type="NewOrderRequest").sort_values(by='TimeStamp')
+order_acknowledges = df.groupby(['OrderID']).apply(lambda x: x[x["MessageType"] == "NewOrderAcknowledged"])
+new_order_requests.reset_index(drop = True, inplace = True)
+order_acknowledges.reset_index(drop = True, inplace = True)
+merged_pd = new_order_requests.merge(order_acknowledges, on="OrderID")[["TimeStamp_x", "TimeStamp_y"]]
+merged_pd["OrderAcknowledgedDuration"] = merged_pd["TimeStamp_y"] - merged_pd["TimeStamp_x"]
+print(merged_pd)
+
+# get id of each order request
+# get order received
