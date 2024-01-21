5cc163acd6bc5e63296b7ef8f4676defd57a92d4
Ali Lezzeik <56936736+aliL123@users.noreply.github.com>
2024-01-20 20:11:29 -0500

    Turned XYDuration into int, working on stock_price

--- graphs/stock_price.py
+++ graphs/stock_price.py
@@ -1,24 +1,17 @@
 import json
 from ingestor import get_data_frame
-from utils import get_trades_by_type
+from utils import get_trades_by_type, get_duration_of_x_and_y
 from dash import Dash, html, dcc, callback, Output, Input, State
 import plotly.express as px
 import pandas as pd
+exchange = "Exchange_2"
+df = get_data_frame(exchange)
 
-df = get_data_frame("Exchange_1")
-
-new_order_requests = get_trades_by_type(df=df, msg_type="CancelRequest").sort_values(by='TimeStamp')
-order_acknowledges = df.groupby(['OrderID']).apply(lambda x: x[x["MessageType"] == "CancelAcknowledged"])
-new_order_requests.reset_index(drop = True, inplace = True)
-order_acknowledges.reset_index(drop = True, inplace = True)
-merged_pd = new_order_requests.merge(order_acknowledges, on="OrderID")[["TimeStamp_x", "TimeStamp_y"]]
-merged_pd["OrderAcknowledgedDuration"] = merged_pd["TimeStamp_y"] - merged_pd["TimeStamp_x"]
-print(merged_pd)
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Input(id='input-stock-state', type='text', value='PRQ83'),
+    dcc.Input(id='input-stock-state', type='text', value='XXRQC'),
     html.Button(id='submit-stock-state', n_clicks=0, children='Submit'),
     dcc.Graph(
         id= 'bubble-stock-id')])
@@ -27,8 +20,14 @@ app.layout = html.Div([
               Input('submit-stock-state', 'n_clicks'),
               State('input-stock-state', 'value'))
 def update_output(n_clicks, stock_value):
+    print(stock_value)
     dff = df[df['Symbol'] == stock_value]
-    fig = px.scatter(merged_pd,x='TimeStamp_x',y="OrderAcknowledgedDuration",size="OrderAcknowledgedDuration")
+    cancel_var = "CancelAcknowledged"
+    if exchange == "Exchange_2":
+        cancel_var = "Canceled"
+    dff = get_duration_of_x_and_y(df,"CancelRequest", cancel_var)
+    print(dff)
+    fig = px.scatter(dff,x='TimeStamp_x',y="XYDuration",color="XYDuration")
     return fig
 
 if __name__ == '__main__':
