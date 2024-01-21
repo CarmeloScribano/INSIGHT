2654e3a5869dfa10e9b1052bc92c32deebc3963e
SevagSA <sevagared2002@gmail.com>
2024-01-20 20:02:28 -0500

    added order id in xy

--- graphs/utils.py
+++ graphs/utils.py
@@ -6,7 +6,7 @@ def get_duration_of_x_and_y(df, msg_x, msg_y):
     msg_Y_df = df.groupby(['OrderID']).apply(lambda x: x[x["MessageType"] == msg_y])
     msg_x_df.reset_index(drop = True, inplace = True)
     msg_Y_df.reset_index(drop = True, inplace = True)
-    merged_pd = msg_x_df.merge(msg_Y_df, on="OrderID")[["TimeStamp_x", "TimeStamp_y"]]
+    merged_pd = msg_x_df.merge(msg_Y_df, on="OrderID")[["OrderID", "TimeStamp_x", "TimeStamp_y"]]
     merged_pd["XYDuration"] = merged_pd["TimeStamp_y"] - merged_pd["TimeStamp_x"]
     return merged_pd
 
