30417e213e2e71c11cea539a10d6920792891b42
Ali Lezzeik <56936736+aliL123@users.noreply.github.com>
2024-01-20 21:24:38 -0500

    Changed default value, fixed plot issue

--- graphs/acked_order_graph.py
+++ graphs/acked_order_graph.py
@@ -4,14 +4,14 @@ from utils import get_duration_of_x_and_y
 from dash import Dash, html, dcc, callback, Output, Input, State
 import plotly.express as px
 import pandas as pd
-exchange = "Exchange_2"
+exchange = "Exchange_1"
 df = get_data_frame(exchange)
 
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Input(id='input-stock-state', type='text', value='XXRQC'),
+    dcc.Input(id='input-stock-state', type='text', value='5AV4I'),
     html.Button(id='submit-stock-state', n_clicks=0, children='Submit'),
     dcc.Graph(
         id= 'bubble-stock-id')])
@@ -23,7 +23,7 @@ app.layout = html.Div([
 def update_output(n_clicks, stock_value):
     dff = df[df['Symbol'] == stock_value]
     dff = get_duration_of_x_and_y(dff,"NewOrderRequest", "NewOrderAcknowledged")
-    fig = px.timeline(dff,x='TimeStamp_x',y="XYDuration",color="XYDuration")
+    fig = px.scatter(dff,x='TimeStamp_x',y="XYDuration",color="XYDuration")
     return fig,stock_value
 
 if __name__ == '__main__':
--- graphs/canceled_order_graph.py
+++ graphs/canceled_order_graph.py
@@ -4,14 +4,14 @@ from utils import get_trades_by_type, get_duration_of_x_and_y
 from dash import Dash, html, dcc, callback, Output, Input, State
 import plotly.express as px
 import pandas as pd
-exchange = "Exchange_2"
+exchange = "Exchange_1"
 df = get_data_frame(exchange)
 
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Input(id='input-stock-state', type='text', value='XXRQC'),
+    dcc.Input(id='input-stock-state', type='text', value='5AV4I'),
     html.Button(id='submit-stock-state', n_clicks=0, children='Submit'),
     dcc.Graph(
         id= 'bubble-stock-id')])
@@ -25,6 +25,7 @@ def update_output(n_clicks, stock_value):
     cancel_var = "CancelAcknowledged"
     if exchange == "Exchange_2":
         cancel_var = "Cancelled"
+
     dff = get_duration_of_x_and_y(dff,"CancelRequest", cancel_var)
     fig = px.scatter(dff,x='TimeStamp_x',y="XYDuration",color="XYDuration")
     return fig,stock_value
